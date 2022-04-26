473e91a8e565b9720cf4c332d12064bb5cdb617d
Kleb <94545082+Spartan2909@users.noreply.github.com>
2022-04-26 09:18:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='KlebLib',
+    version='1.0.0',
+    author='Caleb Robson',
+    author_email='robson.caleb.299@gmail.com',
+    packages=['KlebLib'],
+    url='http://pypi.python.org/pypi/KlebLib/',
+    license='LICENSE',
+    description='A collection of classes and functions that no-one will ever need.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
