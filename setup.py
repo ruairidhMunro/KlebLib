d2fc7f24a03906314c4fda1e4abfcf373a5a52b4
Kleb <94545082+Spartan2909@users.noreply.github.com>
2022-04-26 09:23:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     author='Caleb Robson',
     author_email='robson.caleb.299@gmail.com',
     packages=['KlebLib'],
-    url='http://pypi.python.org/pypi/KlebLib/',
+    url='https://github.com/Spartan2909/KlebLib/',
     license='MIT',
     description='A collection of classes and functions that no-one will ever need.',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8, <4'
 )
